e5c2e93c485c30755a63121385d5199883fd8443
TinyBeer <zhangy254@163.com>
2021-03-14 22:42:24 +0800

    Site updated: 2021-03-14 22:42:23

--- love.js
+++ love.js
@@ -1,6 +1,6 @@
 !function(e,t,a){function r(){for(var e=0;e<s.length;e++)s[e].alpha<=0?(t.body.removeChild(s[e].el),s.splice(e,1)):(s[e].y-
 -,s[e].scale+=.004,s[e].alpha-
-=.013,s[e].el.style.cssText="left:"+s[e].x+"px;top:"+s[e].y+"px;opacity:"+s[e].alpha+";transform:scale("+s[e].scale+","+s[e].scale+") 
+=.013+s[e].el.style.cssText="left:"+s[e].x+"px;top:"+s[e].y+"px;opacity:"+s[e].alpha+";transform:scale("+s[e].scale+","+s[e].scale+") 
 rotate(45deg);background:"+s[e].color+";z-index:99999");requestAnimationFrame(r)}function n(){var t="function"==typeof e.onclick&&e.onclick;e.onclick=function(e){t&&t(),o(e)}}function o(e){var 
 a=t.createElement("div");a.className="heart",s.push({el:a,x:e.clientX-
 5,y:e.clientY-5,scale:1,alpha:1,color:c()}),t.body.appendChild(a)}function i(e){var 
